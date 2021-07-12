8cae11763d4c585d2bc5efd990e291e465e7c9d9
mikelizard5 <mamaya@vmware.com>
2021-07-12 16:35:35 -0500

    wip(added manage component and is mostly functional, but there is an error with ngModel in the input element)

---
+++ src/app/components/manage/manage.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { StocksService } from 'src/app/services/stocks.service';
+@Component({
+  selector: 'manage',
+  templateUrl: './manage.component.html',
+  styleUrls: ['./manage.component.scss']
+})
+export class ManageComponent{
+  symbols: Array<string>;
+  stock: any = '';
+  constructor(private service: StocksService){
+    this.symbols = service.get();
+  }
+  add(){
+    console.log(this.symbols = this.service.add(this.stock.toUpperCase()));
+    this.stock = '';
+  }
+  remove(symbol:any){
+    this.symbols = this.service.remove(symbol);
+  }
+}
