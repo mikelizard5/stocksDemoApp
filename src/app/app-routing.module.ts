77eb62d3589c231ec2dc6503a6c2c9562c7a2478
mikelizard5 <mamaya@vmware.com>
2021-07-13 16:27:31 -0500

    finished basic functionality of stock demo app :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from "@angular/router";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ManageComponent } from "./components/manage/manage.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: DashboardComponent
+    },
+    {
+        path: 'manage',
+        component: ManageComponent
+    }
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const AppRoutingModule = RouterModule.forRoot(routes);
